699ec5af86869c6c92c8a9e45bee7681cd66cdf8
Achmad Damanhuri <139878625+damar009@users.noreply.github.com>
2023-09-06 21:13:55 +0700

    add module css create-event

--- src/component/CreateEvent/CreateNewEvent.jsx
+++ src/component/CreateEvent/CreateNewEvent.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom'
-import './CreateNewEventStyle.css'
+import css from './CreateNewEvent.module.css'
 
 const CreateNewEvent = () => {
     const [isPending, setIsPending] = useState(true);
@@ -16,15 +16,15 @@ const CreateNewEvent = () => {
         e.preventDefault();
         const event = { name, date, time, location, description };
 
-        if (description.length < 100) {
-            setError('Event description must be at least 100 characters.');
+        if (description.length < 20) {
+            setError('Event description must be at least 20 characters.');
             return;
         }
         setError('')
 
         setIsPending(false)
 
-        fetch('http://localhost:8000/events/', {
+        fetch('http://localhost:8001/events/', {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(event)
@@ -40,62 +40,94 @@ const CreateNewEvent = () => {
 
     return (
         <>
-            <h2>Add New Event</h2>
-            <div className="create-new-event">
-
+            <div className={css.createNewEvent}>
+                <h2>Add New Event</h2>
                 <form onSubmit={handleSubmit}>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <label className={css.label}>Event Name:</label>
+                                </td>
+                                <td>
+                                    <input
+                                        className={css.inputForm}
+                                        type="text"
+                                        id="event-name"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label className={css.label}>Select a Date:</label>
+                                </td>
+                                <td>
+                                    <input
+                                        className={css.inputForm}
+                                        type="date"
+                                        id="event-date"
+                                        value={date}
+                                        onChange={(e) => setDate(e.target.value)}
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label className={css.label}>Select a Time:</label>
+                                </td>
+                                <td>
+                                    <input
+                                        className={css.inputForm}
+                                        type="time"
+                                        id="event-time"
+                                        value={time}
+                                        onChange={(e) => setTime(e.target.value)}
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label className={css.label}>Enter Location:</label>
+                                </td>
+                                <td>
+                                    <input
+                                        className={css.inputForm}
+                                        type="text"
+                                        id="location"
+                                        placeholder="E.g., Jakarta Selatan"
+                                        value={location}
+                                        onChange={(e) => setLocation(e.target.value)}
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label className={css.label}>Description:</label>
+                                </td>
+                                <td>
+                                    <textarea
+                                        className={css.inputForm}
+                                        type="text"
+                                        id="description"
+                                        placeholder="Enter a description"
+                                        rows={10}
+                                        value={description}
+                                        onChange={(e) => setDescription(e.target.value)}
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colSpan="2">
+                                    {error && <p>{error}</p>}
+                                    {isPending && <button className={`${css.btn} ${css.addButton}`} >Add Event</button>}
+                                    {!isPending && <button className={`${css.btn} ${css.addingButton}`}>Adding Event...</button>}
 
-                    <label>Event Name:</label>
-                    <input className="input-form-event"
-                        type="text"
-                        id="event-name"
-                        // required
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-
-                    <label>Select a Date:</label>
-                    <input className="input-form-event"
-                        type="date"
-                        id="event-date"
-                        // required
-                        value={date}
-                        onChange={(e) => setDate(e.target.value)}
-                    />
-
-                    <label>Select a Time:</label>
-                    <input className="input-form-event"
-                        type="time"
-                        id="event-time"
-                        // required
-                        value={time}
-                        onChange={(e) => setTime(e.target.value)}
-                    />
-
-                    <label>Enter Location:</label>
-                    <input className="input-form-event"
-                        type="text"
-                        id="location"
-                        // required
-                        placeholder="E.g., Jakarta Selatan"
-                        value={location}
-                        onChange={(e) => setLocation(e.target.value)}
-                    />
-
-                    <label>Description:</label>
-                    <textarea className="input-form-event"
-                        type="text"
-                        id="description"
-                        // required
-                        placeholder="Enter a description"
-                        rows={10}
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                    {error && <p>{error}</p>}
-
-                    {isPending && <button>Add Event</button>}
-                    {!isPending && <button>Adding Event...</button>}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </form>
             </div>
         </>
